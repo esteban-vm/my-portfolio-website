a16285a87991b377f2d2c40ce4434acf7140938a
Esteban V.M. <estebanvm90@outlook.com>
2026-08-23 12:53:25 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} lang='en'>
+    <html className='h-full antialiased' lang='en'>
       <body className='flex min-h-full flex-col'>{children}</body>
     </html>
   )
