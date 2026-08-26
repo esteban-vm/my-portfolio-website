996800a465413df8d9e8b8430713173e8ae292cf
Esteban V.M. <estebanvm90@outlook.com>
2026-08-26 10:13:38 -0300

    add fonts
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/lib/fonts.ts
@@ -0,0 +1,36 @@
+import localFont from 'next/font/local'
+
+export const balsamiq = localFont({
+  variable: '--font-balsamiq',
+  display: 'swap',
+  fallback: ['sans-serif'],
+  src: [
+    { path: '../../public/fonts/Balsamiq-regular.woff2', weight: '400', style: 'normal' },
+    { path: '../../public/fonts/Balsamiq-italic.woff2', weight: '400', style: 'italic' },
+    { path: '../../public/fonts/Balsamiq-bold.woff2', weight: '700', style: 'bold' },
+  ],
+})
+
+export const montserrat = localFont({
+  variable: '--font-montserrat',
+  display: 'swap',
+  fallback: ['sans-serif'],
+  src: [
+    { path: '../../public/fonts/Montserrat-regular.woff2', style: 'normal' },
+    { path: '../../public/fonts/Montserrat-italic.woff2', style: 'italic' },
+  ],
+})
+
+export const saiba45 = localFont({
+  variable: '--font-saiba45',
+  display: 'swap',
+  fallback: ['cursive'],
+  src: '../../public/fonts/SAIBA-45.woff2',
+})
+
+export const neon = localFont({
+  variable: '--font-neon',
+  display: 'swap',
+  fallback: ['sans-serif'],
+  src: '../../public/fonts/Neon.woff2',
+})
