932465eaf5b73111d0956ae9a4ec6994ef706f02
Esteban V.M. <estebanvm90@outlook.com>
2026-08-27 17:40:38 -0300

    update home scene
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/scenes/home.scene.tsx
+++ src/components/scenes/home.scene.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { OrbitControls, PerspectiveCamera, Stars, Stats } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { lazy, Suspense } from 'react'
 import { useMediaQuery } from 'react-responsive'
@@ -17,15 +17,15 @@ export function HomeScene() {
   return (
     <Canvas>
       <Suspense fallback={<ModelLoader />}>
-        <Stats />
-        <Stars fade />
+        {/* <Stats /> */}
+        {/* <Stars fade /> */}
         <City position={[-2, isMD ? -2.5 : -1.5, 2]} rotation={[0, polarAngle, 0]} scale={isMD ? 1 : 1.2} />
         <Car rotation={[0, polarAngle, 0.2]} scale={isMD ? 0.15 : 0.2} />
         <directionalLight color='#d3fc17' intensity={3} position={[0, 1, 0]} />
         <ambientLight intensity={0.5} />
         <PerspectiveCamera makeDefault position={[0, 0, 10]} />
         <OrbitControls
-          autoRotate
+          // autoRotate
           autoRotateSpeed={-0.5}
           enableDamping
           enablePan={false}
