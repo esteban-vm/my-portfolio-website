abcc63b1fd252f517ba869b8c1fb86262d29ae23
Esteban V.M. <estebanvm90@outlook.com>
2026-08-27 11:10:42 -0300

    create model loader
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/common/model-loader.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { Html, useProgress } from '@react-three/drei'
+
+export function ModelLoader() {
+  const { progress } = useProgress()
+  const percent = (progress / 100).toLocaleString('es', { style: 'percent' })
+
+  return (
+    <Html center className='text-center'>
+      <span className='font-bold text-amber-500'>{percent}</span>
+    </Html>
+  )
+}
