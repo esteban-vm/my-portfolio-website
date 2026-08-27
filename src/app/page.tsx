5f25b17ceef05b06e2e63a2152e7d9a03e8fad56
Esteban V.M. <estebanvm90@outlook.com>
2026-08-27 11:11:39 -0300

    update home page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,5 @@
-'use client'
-
-import { OrbitControls, PerspectiveCamera, Stars, Stats } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import { lazy, Suspense } from 'react'
-import { useMediaQuery } from 'react-responsive'
-import { Loader } from '@/components/common'
-
-const City = lazy(() => import('@/components/models').then((mod) => ({ default: mod.CityModel })))
-const Car = lazy(() => import('@/components/models').then((mod) => ({ default: mod.CarModel })))
-
-const polarAngle = Math.PI / 2
+import { HomeScene } from '@/components/scenes'
 
 export default function HomePage() {
-  const isMD = useMediaQuery({ maxWidth: 768 })
-
-  return (
-    <Canvas>
-      <Suspense fallback={<Loader />}>
-        <Stats />
-        <Stars fade />
-        <City position={[-2, isMD ? -2.5 : -1.5, 2]} rotation={[0, polarAngle, 0]} scale={isMD ? 1 : 1.2} />
-        <Car rotation={[0, polarAngle, 0.2]} scale={isMD ? 0.15 : 0.2} />
-        <directionalLight color='#d3fc17' intensity={3} position={[0, 1, 0]} />
-        <ambientLight intensity={0.5} />
-        <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={-0.5}
-          enableDamping
-          enablePan={false}
-          enableZoom={false}
-          maxPolarAngle={polarAngle}
-          minPolarAngle={polarAngle}
-        />
-      </Suspense>
-    </Canvas>
-  )
+  return <HomeScene />
 }
