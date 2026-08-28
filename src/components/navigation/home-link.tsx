b95272fa88f2408f0ddc51c3cb95b69846ece797
Esteban V.M. <estebanvm90@outlook.com>
2026-08-28 09:41:49 -0300

    create home link
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/navigation/home-link.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import tw from 'tailwind-styled-components'
+
+export function HomeLink() {
+  return (
+    <h1>
+      <Link href='/' passHref>
+        <NameLogo>
+          Esteban
+          <LastName>&nbsp;V.M.</LastName>
+        </NameLogo>
+      </Link>
+    </h1>
+  )
+}
+
+const NameLogo = tw.span`logo-shadow fl-text-4xl/7xl pointer-events-auto text-wrap text-center font-saiba45 text-neon-yellow hover:animate-none motion-safe:animate-glitch`
+
+const LastName = tw.span`hidden font-saiba45 md:inline`
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import tw from 'tailwind-styled-components'
+import { HomeLink } from './home-link'
 import { MobileButton } from './mobile-button'
 import { NavLinks } from './nav-links'
 
@@ -7,14 +7,7 @@ export function Navigation() {
   return (
     <Wrapper>
       <InnerWrapper>
-        <h1>
-          <Link href='/' passHref>
-            <NameLogo>
-              Esteban
-              <LastName>&nbsp;V.M.</LastName>
-            </NameLogo>
-          </Link>
-        </h1>
+        <HomeLink />
         <MobileButton />
         <NavLinks />
       </InnerWrapper>
@@ -25,7 +18,3 @@ export function Navigation() {
 const Wrapper = tw.header`container pointer-events-none absolute top-3 left-1/2 z-30 flex h-[11vh] -translate-x-1/2 md:h-[12vh] lg:h-[13vh]`
 
 const InnerWrapper = tw.div`relative flex size-full items-center justify-around`
-
-const NameLogo = tw.span`logo-shadow fl-text-4xl/7xl pointer-events-auto text-wrap text-center font-saiba45 text-neon-yellow hover:animate-none motion-safe:animate-glitch`
-
-const LastName = tw.span`hidden font-saiba45 md:inline`
