3aa950bb2cc7bddc5ed8512ef05b4dd59384aa7f
Esteban V.M. <estebanvm90@outlook.com>
2026-08-28 09:28:49 -0300

    create nav links
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/navigation/nav-links.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import tw from 'tailwind-styled-components'
+import { useUIStore } from '@/hooks'
+import { cn } from '@/lib/ui'
+
+export function NavLinks() {
+  const navbarOpen = useUIStore((s) => s.navbarOpen)
+
+  return <Wrapper className={cn(navbarOpen && 'slide-in-from-top top-full animate-in')}></Wrapper>
+}
+
+const Wrapper = tw.nav`nav-links slide-out-to-top absolute top-[-500%] flex w-[95%] animate-out flex-col items-center justify-around text-nowrap bg-black/70 fill-mode-forwards text-center duration-700 lg:static lg:size-auto lg:animate-none lg:flex-row lg:gap-8 lg:bg-transparent`
