e60bc4133003aaabf9e33c70ec963044a31797c1
Esteban V.M. <estebanvm90@outlook.com>
2026-08-30 08:44:25 -0300

    refactor
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/common/typed-link.tsx
+++ src/components/common/typed-link.tsx
@@ -1,19 +1,10 @@
+import type { ValidRoute } from '@/types'
 import { Link } from '@/i18n/navigation'
-
-type AnySafeSlug = __next_route_internal_types__.SafeSlug<string>
-
-type ExtractValidRoutes<T> = T extends `/${AnySafeSlug}/${infer Rest}`
-  ? `/${Rest}`
-  : T extends `/${AnySafeSlug}`
-    ? '/'
-    : never
-
-type ValidRoutes = ExtractValidRoutes<__next_route_internal_types__.DynamicRoutes>
 
 export type BaseTypedLinkProps = Parameters<typeof Link>[0]
 
 export interface TypedLinkProps extends BaseTypedLinkProps {
-  href: ValidRoutes
+  href: ValidRoute
 }
 
 export function TypedLink(props: TypedLinkProps) {
