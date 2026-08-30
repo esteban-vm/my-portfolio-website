d2e375102ef763afc0379061cd71b6513ab66662
Esteban V.M. <estebanvm90@outlook.com>
2026-08-30 15:47:04 -0300

    nav link
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

---
+++ src/components/navigation/nav-link.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import type { ValidRoute } from '@/types'
+import { usePathname } from 'next/navigation'
+import tw from 'tailwind-styled-components'
+import { useUIStore } from '@/hooks'
+import { cn } from '@/lib/ui'
+import { TypedLink } from '../common'
+
+export interface NavLinkProps {
+  text: string
+  href: ValidRoute
+}
+
+export function NavLink({ text, href }: NavLinkProps) {
+  const pathname = usePathname()
+  const setNavbarOpen = useUIStore((s) => s.setNavbarOpen)
+
+  return (
+    <TypedLink href={href} passHref>
+      <Wrapper
+        className={cn(pathname.includes(href) && 'text-neon-green-dark! after:bg-neon-green-dark!')}
+        onClick={() => setNavbarOpen(false)}
+      >
+        {text}
+      </Wrapper>
+    </TypedLink>
+  )
+}
+
+const Wrapper = tw.span`fl-text-sm/base nav-link pointer-events-auto relative inline-block font-bold font-neon text-neon-yellow uppercase hover:opacity-90 hover:after:origin-bottom-left hover:after:scale-x-100 active:scale-95 lg:after:absolute`
