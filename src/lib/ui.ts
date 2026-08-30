3b3a9109ee7b103d2e84b56dffa45294b9d730ab
Esteban V.M. <estebanvm90@outlook.com>
2026-08-30 15:47:29 -0300

    cleanup
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/ui.ts
+++ src/lib/ui.ts
@@ -1,15 +1,7 @@
 import type { ClassValue } from 'clsx'
-import type { ValidRoute } from '@/types'
 import { clsx } from 'clsx'
 import { twMerge } from 'fluid-tailwindcss/tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function isActive(pathname: string, href: ValidRoute) {
-  if (pathname.includes(href)) {
-    const className = 'text-neon-green-dark! after:bg-neon-green-dark!'
-    return className
-  }
 }
