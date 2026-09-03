1ea428bfe9622ea3e9765801d754114c298835ad
Esteban V.M. <estebanvm90@outlook.com>
2026-09-03 16:19:09 -0300

    create routes constant
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,6 @@
+import type { Route } from 'next'
+
 export const LOCALES = ['en', 'es'] as const
 export const LOCALE_COOKIE = 'NEXT_LOCALE'
+
+export const ROUTES = ['/about', '/contact', '/projects'] as const satisfies Route[]
