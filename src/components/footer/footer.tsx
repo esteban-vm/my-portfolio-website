ad9f82a434c063a28fc0006972586bebb1d5369d
Esteban V.M. <estebanvm90@outlook.com>
2026-09-03 11:26:23 -0300

    fix footer style
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,13 @@
 import tw from 'tailwind-styled-components'
+import { ActionButton } from './action-button'
 
 export function Footer() {
-  return <Wrapper></Wrapper>
+  return (
+    <Wrapper>
+      <ActionButton />
+      <ActionButton />
+    </Wrapper>
+  )
 }
 
-const Wrapper = tw.footer`container pointer-events-none absolute bottom-0 left-1/2 z-20 flex h-(--footer-height) -translate-x-1/2 items-start justify-around bg-neon-green-dark/25`
+const Wrapper = tw.footer`container pointer-events-none absolute bottom-0 left-1/2 z-20 flex h-(--footer-height) -translate-x-1/2 justify-around lg:items-center`
