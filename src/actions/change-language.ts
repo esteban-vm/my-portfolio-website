d13ec84bb0c55d5728efe2bad25b37a49b86b68b
Esteban V.M. <estebanvm90@outlook.com>
2026-09-04 16:50:48 -0300

    update changeLanguage action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/change-language.ts
+++ src/actions/change-language.ts
@@ -5,7 +5,6 @@ import { cookies } from 'next/headers'
 import { LOCALE_COOKIE } from '@/lib/constants'
 
 export async function changeLanguage(locale: Locale) {
-  await new Promise((r) => setTimeout(r, 2_000))
   const cookieStore = await cookies()
   cookieStore.set(LOCALE_COOKIE, locale)
 }
