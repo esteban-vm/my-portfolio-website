756c9b347675e34a68d44a28c0ddfce499f6b4e5
Esteban V.M. <estebanvm90@outlook.com>
2026-09-04 15:23:02 -0300

    update model loader
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/common/model-loader.tsx
+++ src/components/common/model-loader.tsx
@@ -1,14 +1,18 @@
 'use client'
 
 import { Html, useProgress } from '@react-three/drei'
+import { useLocale } from 'next-intl'
+import { PacmanLoader } from 'react-spinners'
 
 export function ModelLoader() {
+  const locale = useLocale()
   const { progress } = useProgress()
-  const percent = (progress / 100).toLocaleString('es', { style: 'percent' })
+  const percent = (progress / 100).toLocaleString(locale, { style: 'percent' })
 
   return (
     <Html center className='text-center'>
-      <span className='font-bold text-amber-500'>{percent}</span>
+      <PacmanLoader className='-left-1/2' color='#ff15bc' speedMultiplier={2} />
+      <div className='mt-2 font-bold text-neon-green-dark'>{percent}</div>
     </Html>
   )
 }
