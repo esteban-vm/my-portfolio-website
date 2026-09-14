22d166693734149dd4558ebf165d9ed9e96edd1b
Esteban V.M. <estebanvm90@outlook.com>
2026-09-14 12:42:01 -0300

    update ui store
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/stores/ui.store.ts
+++ src/stores/ui.store.ts
@@ -1,11 +1,16 @@
 import type { StateCreator } from 'zustand'
+import type { SceneStage } from '@/types'
 
 interface UIState {
   navbarOpen: boolean
+  sceneRotating: boolean
+  currentStage: SceneStage
 }
 
 interface UIActions {
-  setNavbarOpen: (open: boolean) => void
+  setNavbarOpen: (value: boolean) => void
+  setSceneRotating: (value: boolean) => void
+  setCurrentStage: (value: SceneStage) => void
 }
 
 export type UIStore = UIState & UIActions
@@ -13,9 +18,19 @@ export type UIStore = UIState & UIActions
 export const createUIStore: StateCreator<UIStore> = (set) => {
   return {
     navbarOpen: false,
+    sceneRotating: true,
+    currentStage: 1,
 
-    setNavbarOpen(open) {
-      set({ navbarOpen: open })
+    setNavbarOpen(value) {
+      set({ navbarOpen: value })
+    },
+
+    setSceneRotating(value) {
+      set({ sceneRotating: value })
+    },
+
+    setCurrentStage(value) {
+      set({ currentStage: value })
     },
   }
 }
