79b5ef93289873d7d0f555e92d60e3d72f968423
Esteban V.M. <estebanvm90@outlook.com>
2026-09-14 12:40:21 -0300

    update global types
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,1 @@
-type AnySafeSlug = __next_route_internal_types__.SafeSlug<string>
-
-type ExtractValidRoute<T> = T extends `/${AnySafeSlug}/${infer Rest}`
-  ? `/${Rest}`
-  : T extends `/${AnySafeSlug}`
-    ? '/'
-    : never
-
-export type ValidRoute = ExtractValidRoute<__next_route_internal_types__.DynamicRoutes>
+export type SceneStage = 1 | 2 | 3 | 4 | null
