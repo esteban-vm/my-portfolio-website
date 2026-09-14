6f8cfaea3447cc18f7c52a2475bc1a3d9df18f1d
Esteban V.M. <estebanvm90@outlook.com>
2026-09-14 12:40:49 -0300

    update home scene
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/scenes/home.scene.tsx
+++ src/components/scenes/home.scene.tsx
@@ -4,6 +4,7 @@ import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { lazy, Suspense } from 'react'
 import { useMediaQuery } from 'react-responsive'
+import { useUIStore } from '@/hooks'
 import { ModelLoader } from '../common'
 
 const City = lazy(() => import('../models').then((mod) => ({ default: mod.CityModel })))
@@ -13,19 +14,18 @@ const polarAngle = Math.PI / 2
 
 export function HomeScene() {
   const isMD = useMediaQuery({ maxWidth: 768 })
+  const sceneRotating = useUIStore((s) => s.sceneRotating)
 
   return (
     <Canvas>
       <Suspense fallback={<ModelLoader />}>
-        {/* <Stats /> */}
-        {/* <Stars fade /> */}
         <City position={[-2, isMD ? -2.5 : -1.5, 2]} rotation={[0, polarAngle, 0]} scale={isMD ? 1 : 1.2} />
         <Car rotation={[0, polarAngle, 0.2]} scale={isMD ? 0.15 : 0.2} />
         <directionalLight color='#d3fc17' intensity={3} position={[0, 1, 0]} />
         <ambientLight intensity={0.5} />
         <PerspectiveCamera makeDefault position={[0, 0, 10]} />
         <OrbitControls
-          // autoRotate
+          autoRotate={sceneRotating}
           autoRotateSpeed={-0.5}
           enableDamping
           enablePan={false}
