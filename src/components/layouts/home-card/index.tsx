dc5ec8afb85fe570bfc4b6b692e599f128ee89a2
Esteban V.M. <estebanvm90@outlook.com>
2026-09-14 16:08:28 -0300

    update card renderer
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/layouts/home-card/index.tsx
+++ src/components/layouts/home-card/index.tsx
@@ -1,36 +1,22 @@
 'use client'
 
 import type { SceneStage } from '@/types'
+import { useTranslations } from 'next-intl'
 import { useUIStore } from '@/hooks'
 import { HomeCard } from './home-card'
 
 type HomeCardMap = Record<NonNullable<SceneStage>, JSX.Element>
 
-const homeCardMap: HomeCardMap = {
-  '1': (
-    <HomeCard text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dignissim vitae lacus vitae porttitor. Proin nunc arcu, facilisis in nibh sit amet, molestie efficitur dui. Sed rhoncus vulputate neque a ultricies. Maecenas molestie libero id mi fermentum malesuada.' />
-  ),
-  '2': (
-    <HomeCard
-      link='/about'
-      text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel tempora totam quisquam hic debitis illum ducimus facilis dolorum quod voluptate.'
-    />
-  ),
-  '3': (
-    <HomeCard
-      link='/projects'
-      text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat temporibus inventore id laborum maiores numquam perferendis, hic nemo, dolorem magnam, sapiente dolore quisquam consequatur sint.'
-    />
-  ),
-  '4': (
-    <HomeCard
-      link='/contact'
-      text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit quidem temporibus ut accusantium fugit laborum tenetur porro maxime.'
-    />
-  ),
-}
-
 export function HomeCardRenderer() {
+  const t = useTranslations('HomeCards')
   const currentStage = useUIStore((s) => s.currentStage)
+
+  const homeCardMap: HomeCardMap = {
+    '1': <HomeCard text={t('1.text')} />,
+    '2': <HomeCard link='/about' linkText={t('2.linkText')} text={t('2.text')} />,
+    '3': <HomeCard link='/projects' linkText={t('3.linkText')} text={t('3.text')} />,
+    '4': <HomeCard link='/contact' linkText={t('4.linkText')} text={t('4.text')} />,
+  }
+
   return currentStage && homeCardMap[currentStage]
 }
