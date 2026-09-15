24d435b020d5a1632127d0b4faa7325376da6bb8
Esteban V.M. <estebanvm90@outlook.com>
2026-09-15 13:30:46 -0300

    fix style
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(sub pages)/layout.tsx
+++ src/app/(sub pages)/layout.tsx
@@ -6,4 +6,4 @@ export default function SubPagesLayout({ children }: Props) {
   return <Wrapper>{children}</Wrapper>
 }
 
-const Wrapper = tw.div`container mx-auto flex h-screen items-start justify-center py-[14vh] md:py-[15vh] lg:py-[16vh]`
+const Wrapper = tw.div`container mx-auto flex min-h-full items-start justify-center py-[14vh] md:py-[15vh] lg:py-[16vh]`
