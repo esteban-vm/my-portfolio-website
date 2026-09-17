10e32c9b8a7a0e5a49ff94aff3970a38940ba78d
Esteban V.M. <estebanvm90@outlook.com>
2026-09-17 08:28:28 -0300

    update contact form styles
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/components/forms/contact-form/contact-form.styled.ts
+++ src/components/forms/contact-form/contact-form.styled.ts
@@ -13,3 +13,5 @@ export const Input = tw.input`rounded-lg border-2 border-neon-magenta-light p-1.
 export const Textarea = tw.textarea`field-sizing-content min-h-22 resize-none rounded-lg border-2 border-neon-magenta-light p-1.5 text-white outline-neon-magenta-light outline-offset-1 focus:outline-1`
 
 export const Button = tw.button`mt-4 cursor-pointer rounded-lg bg-neon-magenta-light px-3.5 py-1.5 text-white outline-neon-magenta-light outline-offset-1 transition-transform duration-200 hover:opacity-90 not-active:focus:outline-1 active:scale-[98%] disabled:opacity-80 lg:mt-6`
+
+export const Small = tw.small`text-white empty:hidden`
