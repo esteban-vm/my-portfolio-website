cce7046c67804ba2cac1a259e9835e9e237e282c
Esteban V.M. <estebanvm90@outlook.com>
2026-09-17 08:21:07 -0300

    update contact page
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/app/(sub pages)/contact/page.tsx
+++ src/app/(sub pages)/contact/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { getTranslations } from 'next-intl/server'
+import { getLocale, getTranslations } from 'next-intl/server'
 import { ContactForm } from '@/components/forms'
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -10,11 +10,13 @@ export async function generateMetadata(): Promise<Metadata> {
   }
 }
 
-export default function ContactPage() {
+export default async function ContactPage() {
+  const locale = await getLocale()
+
   return (
     <section className='flex flex-col-reverse gap-2.5 p-2.5 lg:flex-row'>
       <div className='flex min-w-[60%] flex-1 flex-col'>
-        <ContactForm />
+        <ContactForm key={locale} />
       </div>
       <div className='min-h-80 flex-1 border border-amber-500'></div>
     </section>
