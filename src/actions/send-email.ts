7e2ef2e862f43a92aecf6fb6ee41d1eef1a2767a
Esteban V.M. <estebanvm90@outlook.com>
2026-09-17 08:44:41 -0300

    update sendEmail action
    Signed-off-by: Esteban V.M. <estebanvm90@outlook.com>

--- src/actions/send-email.ts
+++ src/actions/send-email.ts
@@ -1,18 +1,42 @@
 'use server'
 
-import emailjs, { EmailJSResponseStatus } from '@emailjs/nodejs'
+import emailjs from '@emailjs/nodejs'
+import { getTranslations } from 'next-intl/server'
+import { z } from 'zod'
+import { safeClient } from '@/lib/safe-action'
 
-emailjs.init({
-  publicKey: process.env.EMAILJS_PUBLIC_KEY,
-  privateKey: process.env.EMAILJS_PRIVATE_KEY,
-})
+export const sendEmail = safeClient
+  .inputSchema(async () => {
+    const t = await getTranslations('ContactForm.errors')
 
-export async function sendEmail() {
-  try {
-    const response = await emailjs.send(process.env.EMAILJS_SERVICE_ID!, process.env.EMAILJS_TEMPLATE_ID!)
-    return { success: true, ...response }
-  } catch (error) {
-    if (error instanceof EmailJSResponseStatus) return { success: false, ...error }
-    throw error
-  }
-}
+    return z.object({
+      name: z.string().trim().nonempty(t('name.nonempty')).min(5, t('name.min')).max(50, t('name.max')),
+      email: z.email(t('email')).trim().lowercase(),
+      message: z.string().trim().nonempty(t('message.nonempty')).min(5, t('message.min')).max(255, t('message.max')),
+    })
+  })
+  .outputSchema(z.object({ ok: z.boolean(), message: z.string() }))
+  .action(async ({ parsedInput }) => {
+    const { name, email, message } = parsedInput
+
+    await emailjs.send(
+      process.env.EMAILJS_SERVICE_ID!,
+      process.env.EMAILJS_TEMPLATE_ID!,
+      {
+        from_name: name,
+        to_name: process.env.EMAILJS_TO_NAME,
+        from_email: email,
+        to_email: process.env.EMAILJS_TO_EMAIL,
+        message,
+      },
+      {
+        publicKey: process.env.EMAILJS_PUBLIC_KEY,
+        privateKey: process.env.EMAILJS_PRIVATE_KEY,
+      }
+    )
+
+    return {
+      ok: true,
+      message: 'Mensaje enviado correctamente',
+    }
+  })
